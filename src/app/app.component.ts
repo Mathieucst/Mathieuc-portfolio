5685d24c24bed9f2107598e4189182a4a2e1acbc
Mathieucst <mc281284>
2017-04-13 16:47:51 +0200

    upload 01 init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {selector} from "rxjs/operator/multicast";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+  })
+
+
+
 export class AppComponent {
   title = 'app works!';
 }
